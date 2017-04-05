fdc15ff4ba5968fc155970f3bdbcdde9e6f68aeb
Aaron Evans <aaron.evans@utexas.edu>
2017-04-05 16:19:12 -0500

    Made top bar home button return to home

--- app/react/components/topbar/home_button.js
+++ app/react/components/topbar/home_button.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { browserHistory } from 'react-router-dom';
 
 export default class HomeButton extends React.Component {
+    constructor() {
+        super();
+        this.redirect = this.redirect.bind(this);
+    }
+
+    redirect() {
+        browserHistory.push("/");
+    }
+
     render() {
         return (
             <div className="topbar-button home">   
