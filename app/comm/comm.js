b8b6291fbbb3c442e42935f21c4bf3eaa7f66fe3
Aaron Evans <aaron.ptb@gmail.com>
2017-04-08 14:43:26 -0500

    added vanilla js posting for communication with server after app has loaded

---
+++ app/comm/comm.js
@@ -0,0 +1,13 @@
+export function post(url, data, callback) {
+    xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () { 
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var json = JSON.parse(xhr.responseText);
+            callback(json);
+        }
+    }
+    var data = JSON.stringify(data);
+    xhr.send(data);
+}
