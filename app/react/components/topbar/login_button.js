5e65f3034d64acd6b135016c591c1547c56dac3b
Aaron Evans <aaron.ptb@gmail.com>
2017-04-08 15:18:47 -0500

    added react modal, began work on login/auth from front-end

--- app/react/components/topbar/login_button.js
+++ app/react/components/topbar/login_button.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import store from '../../../redux/store'
+import post from '../../../comm/comm'
 
 export default class Login extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            user: null,
+        }
+    }
+
+    componentDidMount() {
+        if (this.state.user == null) {
+            /*Attempt to verify*/
+            let token = store.getState().token;
+            if (token) { 
+                post("/login", {token: token}, user => this.setState({user: user}));
+            }
+        }
+    }
+
     render() {
+        var current_user = this.state.user;
         return (
-            <div className="top-bar button login">    
+            <div className="topbar-button login">
+                {JSON.stringify(current_user)};
             </div>
         )
     }
