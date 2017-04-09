8d8689cad757a8f227cde5c45401fcac141302d9
Aaron Evans <aaron.ptb@gmail.com>
2017-04-09 15:54:21 -0500

    made post function include token as x-access-header

--- app/comm/comm.js
+++ app/comm/comm.js
@@ -1,7 +1,10 @@
+import store from '../redux/store.js';
+
 function post(url, data, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json");
+    xhr.setRequestHeader("x-access-token", store.getState().token);
     xhr.onreadystatechange = function () { 
         if (xhr.readyState == 4 && xhr.status == 200) {
             var json = JSON.parse(xhr.responseText);
