70cf7490e2f59a31eb6c4447d88a310127c6c9a2
Aaron Evans <aaron.ptb@gmail.com>
2017-04-09 16:03:04 -0500

    fixed redirection bug in visinfo

--- app/react/components/pages/home/vis_info.js
+++ app/react/components/pages/home/vis_info.js
@@ -8,7 +8,7 @@ export default class VisInfo extends React.Component {
 	            <div className="text">
 					<strong className="title">{this.props.title}</strong>
 					<span> by </span>
-					<a className="author" href="/user/{this.props.username}">{this.props.username}</a>
+					<a className="author" href={`/user/${this.props.username}`}>{this.props.username}</a>
 	            </div>
             	<LikeInfo likes={this.props.likes} />
             </div>
