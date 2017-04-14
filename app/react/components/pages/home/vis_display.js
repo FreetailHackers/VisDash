e9d4fec3e046efbdc75a87aff17923e4f3b431b4
jaflo <imjaflo@gmail.com>
2017-04-14 17:19:10 -0500

    Progress on executing p5 code in a panel

--- app/react/components/pages/home/vis_display.js
+++ app/react/components/pages/home/vis_display.js
@@ -2,21 +2,27 @@ import React from 'react';
 // import Visualizer from './magical_p5_shit'
 
 export default class VisDisplay extends React.Component {
+	componentWillReceiveProps(props) {
+		var waiting = setInterval(function() {
+			if (songLoaded) {
+				clearInterval(waiting);
+				var code = props.code;
+				// find all things that might be p5 vars or functions
+				code = code.replace(/(\S+?)\b/gi, function(match, thing) {
+					// if this part of the lib? prefix it if so
+					if (p5things.indexOf(thing) > -1) thing = "p."+thing;
+					return thing;
+				})+'p.setup=function(){var c=p.createCanvas(400,300);c.drop(gotAudio);'
+				+'if(typeof setup=="function")setup()};p.draw=draw;';
+				this.setState({ p5: new p5(new Function("p", code), props.canvasID) });
+			}
+		}, 10);
+	}
 	render() {
-        return (
+		return (
 			<div className="canvas">
 				<canvas id={this.props.canvasID} width="400" height="300"></canvas>
-				<script dangerouslySetInnerHTML={{__html:
-				`
-				console.log("TACOOOOOOOOOOOOS");
-				var canvas = document.getElementById("${this.props.canvasID}");
-				var ctx = canvas.getContext("2d");
-				ctx.fillStyle = "#FF0000";
-				ctx.fillRect(0,0,150,75);
-				`
-			}}>
-				</script>
 			</div>
-        )
-    }
+		)
+	}
 }
