f37f74eda5f6bc7d9480be4844445227b867c8eb
Aaron Evans <aaron.ptb@gmail.com>
2017-04-15 17:40:02 -0500

    buttons now show properly

--- app/react/components/editor/toolbar.js
+++ app/react/components/editor/toolbar.js
@@ -5,19 +5,20 @@ export default class EditorToolbar extends React.Component {
     constructor() {
         super();
         this.state = {
-
         }
     }
 
 	componentDidMount() {
-		console.log(this.props);
+		store.subscribe(() => {
+            this.forceUpdate();
+        })
 	}
 
     render() {
         var canSave = false;
         if (this.props.isShown) {
             var userSubmissions = store.getState().user.submissions;
-            var currSubmissionId = this.props.submissionId;
+            var currSubmissionId = store.getState().submission._id;
             
             for(let sub of userSubmissions) {
                 if (sub._id == currSubmissionId)
