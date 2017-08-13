ddd5c77a241d663d2554ba876687d396542886e0
Aaron Evans <aaron.ptb@gmail.com>
2017-08-13 17:56:44 -0400

    added basic infra for front-end submissions display

---
+++ app/client/views/dashboard/submissions/submissionsCtrl.js
@@ -0,0 +1,16 @@
+angular.module('reg')
+.controller('EditorCtrl', [
+  '$rootScope',
+  '$scope',
+  '$sce',
+  'currentUser',
+  'Utils',
+  'AuthService',
+  'UserService',
+  'EVENT_INFO',
+  function($rootScope, $scope, $sce, currentUser, Utils, AuthService, UserService){
+    var items = []
+
+
+    //ping the server and grab an array of JSON items
+  }]);
--- app/server/routes.js
+++ app/server/routes.js
@@ -2,14 +2,14 @@ var User = require('./models/User');
 
 module.exports = function(app) {
 
-  // Application ------------------------------------------
-  app.get('/', function(req, res){
-    res.sendfile('./app/client/index.html');
-  });
+    // Application ------------------------------------------
+    app.get('/', function(req, res) {
+        res.sendfile('../app/client/index.html');
+    });
 
-  // Wildcard all other GET requests to the angular app
-  app.get('*', function(req, res){
-    res.sendfile('./app/client/index.html');
-  });
+    // Wildcard all other GET requests to the angular app
+    app.get('*', function(req, res) {
+        res.sendfile('./app/client/index.html');
+    });
 
 };
